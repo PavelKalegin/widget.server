88d58e711517f89591267e8c9269761c38bd14df
Pavel Kalegin <PavelKalegin@gmail.com>
2018-01-28 15:18:03 +0100

    Basic

--- src/main/java/app/customer/CustomerController.java
+++ src/main/java/app/customer/CustomerController.java
@@ -3,19 +3,20 @@ package app.customer;
 import app.visitor.VisitorModel;
 import app.visitor.VisitorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class CustomerController {
 
-//    @Autowired
-////    private VisitorService visitorService;
-////
-////    @RequestMapping("app/customer/visitors")
-////    public List<VisitorModel> getAllVisitors(){
-////        return visitorService.getAllVisitors();
-////    }
+    @Autowired
+    private VisitorService visitorService;
+
+    @RequestMapping("app/customer/visitors")
+    public List<VisitorModel> getAllVisitors()
+    {
+        return visitorService.getAllVisitors();
+    }
 }
--- src/main/java/app/visitor/VisitorController.java
+++ src/main/java/app/visitor/VisitorController.java
@@ -1,31 +1,30 @@
 package app.visitor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class VisitorController {
 
     @Autowired
     private VisitorService visitorService;
 
-    //@RequestMapping(method = RequestMethod.POST, value = "/app/visitor")
-    @MessageMapping("/visitor.addVisitor")
-    @SendTo("/customer/visitors")
+    @RequestMapping(method = RequestMethod.POST, value = "/app/visitor")
     public List<VisitorModel> addVisitor(VisitorModel visitorModel)
     {
         visitorService.addVisitorModel(visitorModel);
         return visitorService.getAllVisitors();
     }
 
-//    @RequestMapping(method = RequestMethod.DELETE,value = "/app/visitor/{id}")
-//    public void deleteVisitor(@PathVariable String id)
-//    {
-//        visitorService.deleteVisitorModel(id);
-//    }
+    @RequestMapping(method = RequestMethod.DELETE,value = "/app/visitor/{id}")
+    public void deleteVisitor(@PathVariable String id)
+    {
+        visitorService.deleteVisitorModel(id);
+    }
 }
