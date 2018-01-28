d267a6df5fe838d4bec3821e55738ec5f75ef2f7
Pavel Kalegin <PavelKalegin@gmail.com>
2018-01-28 17:01:07 +0100

    WebSocket 1.0

--- src/main/java/app/visitor/VisitorController.java
+++ src/main/java/app/visitor/VisitorController.java
@@ -2,6 +2,9 @@ package app.visitor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,22 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+
+@Controller
 public class VisitorController {
 
     @Autowired
     private VisitorService visitorService;
 
-    @RequestMapping(method = RequestMethod.POST, value = "/app/visitor")
-    public List<VisitorModel> addVisitor(VisitorModel visitorModel)
+    @MessageMapping("visitor.add")
+    @SendTo("/customer/visitors")
+    public VisitorModel addVisitor(VisitorModel visitorModel)
     {
         visitorService.addVisitorModel(visitorModel);
-        return visitorService.getAllVisitors();
-    }
-
-    @RequestMapping(method = RequestMethod.DELETE,value = "/app/visitor/{id}")
-    public void deleteVisitor(@PathVariable String id)
-    {
-        visitorService.deleteVisitorModel(id);
+        return visitorModel;
     }
 }
