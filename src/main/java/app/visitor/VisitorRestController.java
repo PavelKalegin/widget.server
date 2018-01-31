b0d694be6d2d057bb09b14196d95ef4b9a13c66c
Pavel Kalegin <PavelKalegin@gmail.com>
2018-01-31 22:16:26 +0100

    add room logic

--- src/main/java/app/visitor/VisitorRestController.java
+++ src/main/java/app/visitor/VisitorRestController.java
@@ -4,19 +4,25 @@ import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
 @RestController
-//@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("app/visitor")
 public class VisitorRestController {
     @Autowired
     private VisitorService visitorService;
 
-    @RequestMapping(method = RequestMethod.POST,value = "app/visitor/{roomID}")
-    public void connect(@PathVariable String roomID, @RequestBody Map<String,String> parameters, HttpServletRequest request)
+    @PostMapping("/{roomID}")
+    public @ResponseBody Map<String,String> connect(@PathVariable String roomID, @RequestBody Map<String,String> parameters, HttpServletRequest request)
     {
+
         visitorService.addVisitorModel(new VisitorModel(request.getSession().getId(),parameters.get("name"), roomID));
+        Map<String, String> response = new HashMap<>();
+        response.put("id",request.getSession().getId());
+        return response;
     }
 
 }
